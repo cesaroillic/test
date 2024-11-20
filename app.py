9af333f18fc284597b3b9288293be9cfc6140ee0
cesaroillic <cesar.oillic@se.com>
2024-11-20 17:37:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-import streamlit as st 
+import streamlit as st
 
+# Fonction pour créer un fichier .txt
+def create_file():
+    with open("new_file.txt", "w") as file:
+        file.write("This is a new file created by Streamlit.")
+
+# Application Streamlit
 st.write('hello')
+
+# Ajouter un bouton pour créer le fichier
+if st.button('Create File'):
+    create_file()
+    st.success('File created successfully!')
